e23ab73374ae3b8a79650b17921bdba8d62d7993
Edgar Omar Cruz Velasco <Ecruz@Idear.local>
2022-02-28 11:28:58 -0600

    Mi primer comit

---
+++ src/piramide/Piramide.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package piramide;
+
+public class Piramide {
+public static int numero =10;
+   
+    public static void main(String[] args) {
+        String cadena ="*";
+        String resultado ="";
+        
+        for (int i = 1; i <=numero ; i++){
+            resultado = resultado + cadena;
+            System.out.println(resultado);
+            }
+            
+        
+        
+        
+        
+    }
+    
+}
